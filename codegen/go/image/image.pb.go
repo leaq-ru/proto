c2fb87d0084d6e734a18a9be939bfbc593f35e7d
nnqq <43930873+nnqq@users.noreply.github.com>
2020-11-07 16:25:39 +0300

    wip

--- codegen/go/image/image.pb.go
+++ codegen/go/image/image.pb.go
@@ -30,6 +30,53 @@ const (
 // of the legacy proto package is being used.
 const _ = proto.ProtoPackageIsVersion4
 
+type PutBase64Request struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Base64 string `protobuf:"bytes,1,opt,name=base64,proto3" json:"base64,omitempty"`
+}
+
+func (x *PutBase64Request) Reset() {
+	*x = PutBase64Request{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_image_image_proto_msgTypes[0]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *PutBase64Request) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*PutBase64Request) ProtoMessage() {}
+
+func (x *PutBase64Request) ProtoReflect() protoreflect.Message {
+	mi := &file_image_image_proto_msgTypes[0]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use PutBase64Request.ProtoReflect.Descriptor instead.
+func (*PutBase64Request) Descriptor() ([]byte, []int) {
+	return file_image_image_proto_rawDescGZIP(), []int{0}
+}
+
+func (x *PutBase64Request) GetBase64() string {
+	if x != nil {
+		return x.Base64
+	}
+	return ""
+}
+
 type PutRequest struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -41,7 +88,7 @@ type PutRequest struct {
 func (x *PutRequest) Reset() {
 	*x = PutRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_image_image_proto_msgTypes[0]
+		mi := &file_image_image_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -54,7 +101,7 @@ func (x *PutRequest) String() string {
 func (*PutRequest) ProtoMessage() {}
 
 func (x *PutRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_image_image_proto_msgTypes[0]
+	mi := &file_image_image_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -67,7 +114,7 @@ func (x *PutRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use PutRequest.ProtoReflect.Descriptor instead.
 func (*PutRequest) Descriptor() ([]byte, []int) {
-	return file_image_image_proto_rawDescGZIP(), []int{0}
+	return file_image_image_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *PutRequest) GetUrl() string {
@@ -88,7 +135,7 @@ type PutResponse struct {
 func (x *PutResponse) Reset() {
 	*x = PutResponse{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_image_image_proto_msgTypes[1]
+		mi := &file_image_image_proto_msgTypes[2]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -101,7 +148,7 @@ func (x *PutResponse) String() string {
 func (*PutResponse) ProtoMessage() {}
 
 func (x *PutResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_image_image_proto_msgTypes[1]
+	mi := &file_image_image_proto_msgTypes[2]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -114,7 +161,7 @@ func (x *PutResponse) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use PutResponse.ProtoReflect.Descriptor instead.
 func (*PutResponse) Descriptor() ([]byte, []int) {
-	return file_image_image_proto_rawDescGZIP(), []int{1}
+	return file_image_image_proto_rawDescGZIP(), []int{2}
 }
 
 func (x *PutResponse) GetS3Url() string {
@@ -135,7 +182,7 @@ type RemoveRequest struct {
 func (x *RemoveRequest) Reset() {
 	*x = RemoveRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_image_image_proto_msgTypes[2]
+		mi := &file_image_image_proto_msgTypes[3]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -148,7 +195,7 @@ func (x *RemoveRequest) String() string {
 func (*RemoveRequest) ProtoMessage() {}
 
 func (x *RemoveRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_image_image_proto_msgTypes[2]
+	mi := &file_image_image_proto_msgTypes[3]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -161,7 +208,7 @@ func (x *RemoveRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use RemoveRequest.ProtoReflect.Descriptor instead.
 func (*RemoveRequest) Descriptor() ([]byte, []int) {
-	return file_image_image_proto_rawDescGZIP(), []int{2}
+	return file_image_image_proto_rawDescGZIP(), []int{3}
 }
 
 func (x *RemoveRequest) GetS3Url() string {
@@ -177,24 +224,31 @@ var file_image_image_proto_rawDesc = []byte{
 	0x0a, 0x11, 0x69, 0x6d, 0x61, 0x67, 0x65, 0x2f, 0x69, 0x6d, 0x61, 0x67, 0x65, 0x2e, 0x70, 0x72,
 	0x6f, 0x74, 0x6f, 0x12, 0x05, 0x69, 0x6d, 0x61, 0x67, 0x65, 0x1a, 0x1b, 0x67, 0x6f, 0x6f, 0x67,
 	0x6c, 0x65, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2f, 0x65, 0x6d, 0x70, 0x74,
-	0x79, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x1e, 0x0a, 0x0a, 0x50, 0x75, 0x74, 0x52, 0x65,
-	0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x10, 0x0a, 0x03, 0x75, 0x72, 0x6c, 0x18, 0x01, 0x20, 0x01,
-	0x28, 0x09, 0x52, 0x03, 0x75, 0x72, 0x6c, 0x22, 0x24, 0x0a, 0x0b, 0x50, 0x75, 0x74, 0x52, 0x65,
-	0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x15, 0x0a, 0x06, 0x73, 0x33, 0x5f, 0x75, 0x72, 0x6c,
-	0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x73, 0x33, 0x55, 0x72, 0x6c, 0x22, 0x26, 0x0a,
-	0x0d, 0x52, 0x65, 0x6d, 0x6f, 0x76, 0x65, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x15,
-	0x0a, 0x06, 0x73, 0x33, 0x5f, 0x75, 0x72, 0x6c, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05,
-	0x73, 0x33, 0x55, 0x72, 0x6c, 0x32, 0x6d, 0x0a, 0x05, 0x49, 0x6d, 0x61, 0x67, 0x65, 0x12, 0x2c,
-	0x0a, 0x03, 0x50, 0x75, 0x74, 0x12, 0x11, 0x2e, 0x69, 0x6d, 0x61, 0x67, 0x65, 0x2e, 0x50, 0x75,
-	0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x12, 0x2e, 0x69, 0x6d, 0x61, 0x67, 0x65,
-	0x2e, 0x50, 0x75, 0x74, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x36, 0x0a, 0x06,
-	0x52, 0x65, 0x6d, 0x6f, 0x76, 0x65, 0x12, 0x14, 0x2e, 0x69, 0x6d, 0x61, 0x67, 0x65, 0x2e, 0x52,
-	0x65, 0x6d, 0x6f, 0x76, 0x65, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x16, 0x2e, 0x67,
-	0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x45,
-	0x6d, 0x70, 0x74, 0x79, 0x42, 0x2c, 0x5a, 0x2a, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63,
-	0x6f, 0x6d, 0x2f, 0x6e, 0x6e, 0x71, 0x71, 0x2f, 0x73, 0x63, 0x72, 0x2d, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x2f, 0x63, 0x6f, 0x64, 0x65, 0x67, 0x65, 0x6e, 0x2f, 0x67, 0x6f, 0x2f, 0x69, 0x6d, 0x61,
-	0x67, 0x65, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x79, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x2a, 0x0a, 0x10, 0x50, 0x75, 0x74, 0x42, 0x61,
+	0x73, 0x65, 0x36, 0x34, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x16, 0x0a, 0x06, 0x62,
+	0x61, 0x73, 0x65, 0x36, 0x34, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x62, 0x61, 0x73,
+	0x65, 0x36, 0x34, 0x22, 0x1e, 0x0a, 0x0a, 0x50, 0x75, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73,
+	0x74, 0x12, 0x10, 0x0a, 0x03, 0x75, 0x72, 0x6c, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x03,
+	0x75, 0x72, 0x6c, 0x22, 0x24, 0x0a, 0x0b, 0x50, 0x75, 0x74, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e,
+	0x73, 0x65, 0x12, 0x15, 0x0a, 0x06, 0x73, 0x33, 0x5f, 0x75, 0x72, 0x6c, 0x18, 0x01, 0x20, 0x01,
+	0x28, 0x09, 0x52, 0x05, 0x73, 0x33, 0x55, 0x72, 0x6c, 0x22, 0x26, 0x0a, 0x0d, 0x52, 0x65, 0x6d,
+	0x6f, 0x76, 0x65, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x15, 0x0a, 0x06, 0x73, 0x33,
+	0x5f, 0x75, 0x72, 0x6c, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x73, 0x33, 0x55, 0x72,
+	0x6c, 0x32, 0xa7, 0x01, 0x0a, 0x05, 0x49, 0x6d, 0x61, 0x67, 0x65, 0x12, 0x2c, 0x0a, 0x03, 0x50,
+	0x75, 0x74, 0x12, 0x11, 0x2e, 0x69, 0x6d, 0x61, 0x67, 0x65, 0x2e, 0x50, 0x75, 0x74, 0x52, 0x65,
+	0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x12, 0x2e, 0x69, 0x6d, 0x61, 0x67, 0x65, 0x2e, 0x50, 0x75,
+	0x74, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x38, 0x0a, 0x09, 0x50, 0x75, 0x74,
+	0x42, 0x61, 0x73, 0x65, 0x36, 0x34, 0x12, 0x17, 0x2e, 0x69, 0x6d, 0x61, 0x67, 0x65, 0x2e, 0x50,
+	0x75, 0x74, 0x42, 0x61, 0x73, 0x65, 0x36, 0x34, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a,
+	0x12, 0x2e, 0x69, 0x6d, 0x61, 0x67, 0x65, 0x2e, 0x50, 0x75, 0x74, 0x52, 0x65, 0x73, 0x70, 0x6f,
+	0x6e, 0x73, 0x65, 0x12, 0x36, 0x0a, 0x06, 0x52, 0x65, 0x6d, 0x6f, 0x76, 0x65, 0x12, 0x14, 0x2e,
+	0x69, 0x6d, 0x61, 0x67, 0x65, 0x2e, 0x52, 0x65, 0x6d, 0x6f, 0x76, 0x65, 0x52, 0x65, 0x71, 0x75,
+	0x65, 0x73, 0x74, 0x1a, 0x16, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f,
+	0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x42, 0x2c, 0x5a, 0x2a, 0x67,
+	0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6e, 0x6e, 0x71, 0x71, 0x2f, 0x73,
+	0x63, 0x72, 0x2d, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x63, 0x6f, 0x64, 0x65, 0x67, 0x65, 0x6e,
+	0x2f, 0x67, 0x6f, 0x2f, 0x69, 0x6d, 0x61, 0x67, 0x65, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f,
+	0x33,
 }
 
 var (
@@ -209,20 +263,23 @@ func file_image_image_proto_rawDescGZIP() []byte {
 	return file_image_image_proto_rawDescData
 }
 
-var file_image_image_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
+var file_image_image_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_image_image_proto_goTypes = []interface{}{
-	(*PutRequest)(nil),    // 0: image.PutRequest
-	(*PutResponse)(nil),   // 1: image.PutResponse
-	(*RemoveRequest)(nil), // 2: image.RemoveRequest
-	(*empty.Empty)(nil),   // 3: google.protobuf.Empty
+	(*PutBase64Request)(nil), // 0: image.PutBase64Request
+	(*PutRequest)(nil),       // 1: image.PutRequest
+	(*PutResponse)(nil),      // 2: image.PutResponse
+	(*RemoveRequest)(nil),    // 3: image.RemoveRequest
+	(*empty.Empty)(nil),      // 4: google.protobuf.Empty
 }
 var file_image_image_proto_depIdxs = []int32{
-	0, // 0: image.Image.Put:input_type -> image.PutRequest
-	2, // 1: image.Image.Remove:input_type -> image.RemoveRequest
-	1, // 2: image.Image.Put:output_type -> image.PutResponse
-	3, // 3: image.Image.Remove:output_type -> google.protobuf.Empty
-	2, // [2:4] is the sub-list for method output_type
-	0, // [0:2] is the sub-list for method input_type
+	1, // 0: image.Image.Put:input_type -> image.PutRequest
+	0, // 1: image.Image.PutBase64:input_type -> image.PutBase64Request
+	3, // 2: image.Image.Remove:input_type -> image.RemoveRequest
+	2, // 3: image.Image.Put:output_type -> image.PutResponse
+	2, // 4: image.Image.PutBase64:output_type -> image.PutResponse
+	4, // 5: image.Image.Remove:output_type -> google.protobuf.Empty
+	3, // [3:6] is the sub-list for method output_type
+	0, // [0:3] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -235,7 +292,7 @@ func file_image_image_proto_init() {
 	}
 	if !protoimpl.UnsafeEnabled {
 		file_image_image_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*PutRequest); i {
+			switch v := v.(*PutBase64Request); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -247,7 +304,7 @@ func file_image_image_proto_init() {
 			}
 		}
 		file_image_image_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*PutResponse); i {
+			switch v := v.(*PutRequest); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -259,6 +316,18 @@ func file_image_image_proto_init() {
 			}
 		}
 		file_image_image_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*PutResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_image_image_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*RemoveRequest); i {
 			case 0:
 				return &v.state
@@ -277,7 +346,7 @@ func file_image_image_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_image_image_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   3,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
@@ -304,6 +373,7 @@ const _ = grpc.SupportPackageIsVersion6
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type ImageClient interface {
 	Put(ctx context.Context, in *PutRequest, opts ...grpc.CallOption) (*PutResponse, error)
+	PutBase64(ctx context.Context, in *PutBase64Request, opts ...grpc.CallOption) (*PutResponse, error)
 	Remove(ctx context.Context, in *RemoveRequest, opts ...grpc.CallOption) (*empty.Empty, error)
 }
 
@@ -324,6 +394,15 @@ func (c *imageClient) Put(ctx context.Context, in *PutRequest, opts ...grpc.Call
 	return out, nil
 }
 
+func (c *imageClient) PutBase64(ctx context.Context, in *PutBase64Request, opts ...grpc.CallOption) (*PutResponse, error) {
+	out := new(PutResponse)
+	err := c.cc.Invoke(ctx, "/image.Image/PutBase64", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 func (c *imageClient) Remove(ctx context.Context, in *RemoveRequest, opts ...grpc.CallOption) (*empty.Empty, error) {
 	out := new(empty.Empty)
 	err := c.cc.Invoke(ctx, "/image.Image/Remove", in, out, opts...)
@@ -336,6 +415,7 @@ func (c *imageClient) Remove(ctx context.Context, in *RemoveRequest, opts ...grp
 // ImageServer is the server API for Image service.
 type ImageServer interface {
 	Put(context.Context, *PutRequest) (*PutResponse, error)
+	PutBase64(context.Context, *PutBase64Request) (*PutResponse, error)
 	Remove(context.Context, *RemoveRequest) (*empty.Empty, error)
 }
 
@@ -345,6 +425,9 @@ type UnimplementedImageServer struct {
 
 func (*UnimplementedImageServer) Put(context.Context, *PutRequest) (*PutResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Put not implemented")
+}
+func (*UnimplementedImageServer) PutBase64(context.Context, *PutBase64Request) (*PutResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method PutBase64 not implemented")
 }
 func (*UnimplementedImageServer) Remove(context.Context, *RemoveRequest) (*empty.Empty, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Remove not implemented")
@@ -368,6 +451,24 @@ func _Image_Put_Handler(srv interface{}, ctx context.Context, dec func(interface
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(ImageServer).Put(ctx, req.(*PutRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Image_PutBase64_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(PutBase64Request)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ImageServer).PutBase64(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/image.Image/PutBase64",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ImageServer).PutBase64(ctx, req.(*PutBase64Request))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -397,6 +498,10 @@ var _Image_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Put",
 			Handler:    _Image_Put_Handler,
+		},
+		{
+			MethodName: "PutBase64",
+			Handler:    _Image_PutBase64_Handler,
 		},
 		{
 			MethodName: "Remove",
